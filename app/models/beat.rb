39763b6cf5168e4e517d6a7d63d5c1f198be8b9c
Sugishin073 <buckesbeats@gmail.com>
2024-11-20 18:22:08 +0900

    wavの追加

--- app/models/beat.rb
+++ app/models/beat.rb
@@ -3,4 +3,14 @@ class Beat < ApplicationRecord
 
   validates :title, presence: true
   validates :audio_file, presence: true
+  validate :correct_audio_mime_type
+
+  private
+
+  # MIMEタイプのバリデーションを追加
+  def correct_audio_mime_type
+    if audio_file.attached? && !audio_file.content_type.in?(%w[audio/mpeg audio/wav audio/x-wav])
+      errors.add(:audio_file, "はMP3またはWAV形式のみアップロードできます")
+    end
+  end
 end
