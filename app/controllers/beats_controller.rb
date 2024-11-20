d2bd92245360495c3192758d12636410f793c91c
Sugishin073 <buckesbeats@gmail.com>
2024-11-20 17:15:57 +0900

    ビーツコントローラー

---
+++ app/controllers/beats_controller.rb
@@ -0,0 +1,25 @@
+class BeatsController < ApplicationController
+  def index
+    @beats = Beat.all
+  end
+
+  def new
+    @beat = Beat.new
+  end
+
+  def create
+    @beat = Beat.new(beat_params)
+
+    if @beat.save
+      redirect_to beats_path, notice: "ビートがアップロードされました。"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def beat_params
+    params.require(:beat).permit(:title, :audio_file)
+  end
+end
