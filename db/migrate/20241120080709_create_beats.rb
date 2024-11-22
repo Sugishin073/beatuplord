15e4a9bfc240030f24adc34698468612684d9249
Sugishin073 <buckesbeats@gmail.com>
2024-11-22 15:25:02 +0900

    カラム編集

--- db/migrate/20241120080709_create_beats.rb
+++ db/migrate/20241120080709_create_beats.rb
@@ -2,7 +2,9 @@ class CreateBeats < ActiveRecord::Migration[7.0]
   def change
     create_table :beats do |t|
       t.string :title
-
+      t.references :user, null: false, foreign_key: true
+      t.integer    :genre_id, null: false
+      t.integer    :vibes_id , null: false
       t.timestamps
     end
   end
